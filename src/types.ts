7a556fdd734b00bae17bb6e18bb8ff242c4fde02
Nevold <Nevold@tut.by>
2025-05-09 18:45:11 +0300

    add

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import { IncomingMessage, ServerResponse } from 'http';
 
 export interface Item {
   id: string;
-  name: string;
+  name?: string;
   description?: string;
   [key: string]: unknown;
 }
